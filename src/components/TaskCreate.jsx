2591c16218c1599fef28351040f9a901dd1ee399
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-04-22 02:30:19 +0300

    refresh-commit

--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "../App.css";
 
-const TaskCreate = ({ onCreate }) => {
-  const [title, setTitle] = useState(" ");
-  const [taskDesc, setTaskDesc] = useState(" ");
+const TaskCreate = ({ onCreate, task, taskFormUpdate }) => {
+  const [title, setTitle] = useState(task ? task.title : "");
+  const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : "");
 
   const handleChange = (e) => {
     setTitle(e.target.value);
@@ -19,32 +19,68 @@ const TaskCreate = ({ onCreate }) => {
   };
 
   return (
-    <div className="task-create">
-      <h3>Lütfen Task Ekleyiniz!!</h3>
-      <form className="task-form" onSubmit={handleSubmit}>
-        <label className="task-label" htmlFor="baslik">
-          Başlık
-        </label>
-        <input
-          type="text"
-          className="task-input"
-          id="baslik"
-          value={title}
-          onChange={handleChange}
-        />
-        <label className="task-label" htmlFor="olustur">
-          Task Giriniz
-        </label>
-        <textarea
-          type="text"
-          className="task-input"
-          id="olustur"
-          rows={5}
-          value={taskDesc}
-          onChange={handleTaskChange}
-        />
-        <button className="task-button">Oluştur</button>
-      </form>
+    <div>
+      {taskFormUpdate ? (
+        <>
+          <div className="task-update">
+            <h3>Lütfen Taskı Düzenleyiniz!!</h3>
+            <form className="task-form" onSubmit={handleSubmit}>
+              <label className="task-label" htmlFor="baslik">
+                Başlığı Düzenleyiniz
+              </label>
+              <input
+                type="text"
+                className="task-input"
+                id="baslik"
+                value={title}
+                onChange={handleChange}
+              />
+              <label className="task-label" htmlFor="olustur">
+                Taskı Düzenleyiniz
+              </label>
+              <textarea
+                type="text"
+                className="task-input"
+                id="olustur"
+                rows={5}
+                value={taskDesc}
+                onChange={handleTaskChange}
+              />
+              <button className="task-button update-button">Güncelle</button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="task-create">
+            <h3>Lütfen Task Ekleyiniz!!</h3>
+            <form className="task-form" onSubmit={handleSubmit}>
+              <label className="task-label" htmlFor="baslik">
+                Başlık
+              </label>
+              <input
+                type="text"
+                className="task-input"
+                id="baslik"
+                value={title}
+                onChange={handleChange}
+              />
+              <label className="task-label" htmlFor="olustur">
+                Task Giriniz
+              </label>
+              <textarea
+                type="text"
+                className="task-input"
+                id="olustur"
+                rows={5}
+                value={taskDesc}
+                onChange={handleTaskChange}
+              />
+              <button className="task-button">Oluştur</button>
+            </form>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/TaskShow.jsx
+++ src/components/TaskShow.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskCreate from "./TaskCreate";
 
 const TaskShow = ({ task, index, onDelete }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
   const handleDeleteClick = () => {
     onDelete(task.id);
   };
-  console.log(task);
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
   return (
     <div className="task-show">
-      <h3 className="task-title">{index + 1}. Göreviniz</h3>
-      <p>{task.title}</p>
-      <h3 className="task-title">Yapılacaklar</h3>
-      <p>{task.taskDesc}</p>
-      <div>
-        <button className="task-delete" onClick={handleDeleteClick}>
-          Sil
-        </button>
-        <button className="task-update">Güncelle</button>
-      </div>
+      {showEdit ? (
+        <>
+          <TaskCreate task={task} taskFormUpdate={true} />
+        </>
+      ) : (
+        <>
+          <h3 className="task-title">{index + 1}. Göreviniz</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Yapılacaklar</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              Sil
+            </button>
+            <button className="task-update-button" onClick={handleEditClick}>
+              Güncelle
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
